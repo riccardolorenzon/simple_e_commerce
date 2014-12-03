0f8e7fdd3524adaf5a458895f0710a2b7f812536
riccardoL <riccardo.lorenzon@gmai.com>
2014-12-04 00:14:23 +0100

    views template model

--- simpleECommerce/urls.py
+++ simpleECommerce/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from ecommerce import urls as app_urls
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'simpleECommerce.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include(app_urls)),
     url(r'^admin/', include(admin.site.urls)),
+
 )
