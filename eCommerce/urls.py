944fe2ea1e67163fe3d0c4fc67baae304be64d8a
riccardoL <riccardo.lorenzon@gmai.com>
2014-12-04 21:44:51 +0100

    catalog
    catalog

---
+++ eCommerce/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from ecommerce import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'simpleECommerce.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.catalog, name ="catalog"),
+
+)
--- eCommerce/views.py
+++ eCommerce/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import Product
 
-# Create your views here.
+def cartItems(cart):
+    items = []
+    for item in items:
+        items.append(Product.objects.get(id = item))
+    return items
+
+def priceCart(cart):
+    cart_items = cartItems(cart)
+    price = 0
+    for item in cart_items:
+        price += item.price
+    return price
+
+def catalog(request):
+    if 'cart' not in request.session:
+        request.session['cart'] = []
+    cart = request.session['cart']
+    request.session.set_expiry(0)
+    store_items = Product.objects.all()
+
+    ctx = {'store_items' : store_items, 'cart_items' : len(cart)}
+    if request.method == 'POST':
+        cart.append(int(request.POST['obj_id']))
+        return redirect('catalog')
+    return render(request, "catalog.html", ctx)
+
+def cart(request):
+    cart = request.session['cart']
+    request.session.set_expiry(0)
+    ctx = {'cart' : cart, 'cart_size' : len(cart), 'cart_items': cartItems(cart),
+           'total_price' : priceCart(cart)}
+    return render(request, "cart.html", ctx)
